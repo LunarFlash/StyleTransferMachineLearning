637e5e254cb8657c53a05485a3e6955458c9cb21
Yi Wang <yiwang1982@gmail.com>
2018-11-09 15:07:16 -0500

    fixed a bug due to miss typing

--- StyleTransferFun/ViewController.swift
+++ StyleTransferFun/ViewController.swift
@@ -44,16 +44,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
         }
     }
-    
-    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        // The info dictionary may contain multiple representations of the image. You want to use the original.
-        guard let selectedImage = info[.originalImage] as? UIImage else {
-            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
-        }
-        imageView.image = selectedImage
-        dismiss(animated: true, completion: nil)
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            // The info dictionary may contain multiple representations of the image. You want to use the original.
+            guard let selectedImage = info[.originalImage] as? UIImage else {
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+            }
+            imageView.image = selectedImage
+            dismiss(animated: true, completion: nil)
     }
-    
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
