c1a35e99791692ca0564b207c8e9c0b07d233963
Terry Wang <Terry@administrators-iMac.local>
2018-11-09 10:35:43 -0500

    initial commit

---
+++ StyleTransferFun/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Style Transfer Starter
+//
+//  Created by Sai Kambampati on 7/7/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imageView.layer.masksToBounds = true
+    }
+
+    @IBAction func chooseImage(_ sender: Any) {
+        // Choose Image Here
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func transformImage(_ sender: Any) {
+        // Style Transfer Here
+        
+    }
+    
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        imageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
